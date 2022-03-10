9f6c81b266e4b24df8fe6f7fc0b1dd366d41f5cf
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-03-10 14:06:30 +0100

    Hello vue

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#containerApp',
+    data: {
+        title: 'Foto di Lorem Picsum',
+        scrImage: 'https://picsum.photos/400/300',
+        altImage: 'Foto di Lorem Picsum',
+    },
+
+})
