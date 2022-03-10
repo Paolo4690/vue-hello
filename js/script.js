533185d83bee25be158eee1f5f0b4a822b3e53c8
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-03-10 14:18:56 +0100

    add button

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ const app = new Vue({
     el: '#containerApp',
     data: {
         title: 'Foto di Lorem Picsum',
-        scrImage: 'https://picsum.photos/400/300',
+        srcImage: 'https://picsum.photos/400/300',
         altImage: 'Foto di Lorem Picsum',
     },
-
-})
+    methods: {
+        reload: function() {
+            location.reload()
+        }
+    }
+});
